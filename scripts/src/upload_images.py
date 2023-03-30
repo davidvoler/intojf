0f9e0974e1f2514e9adc6740bb30bf8fcc7550a6
david <davidvoler@gmail.com>
2023-03-30 17:19:08 +0300

    test

--- scripts/src/upload_images.py
+++ scripts/src/upload_images.py
@@ -11,6 +11,8 @@ REPO_LIST = [
 def create_release(release: str):
     client = docker.from_env()
     images = docker.images.list()
-
-    for img in images:
-        client.images.push('myimage', tag='latest')
+    for repo in REPO_LIST:
+        image = f"{REGISTRY}/{repo}"
+        docker.images.pull(image, tag="latest")
+        docker.images.tag(image, tag=release)
+        docker.images.push(image, tag=release)
