ca2cec4b6f048c964de873081e1dcc12069db603
david <davidvoler@gmail.com>
2023-03-30 17:58:35 +0300

    adding lib

---
+++ npm_packages/ngworkspace/projects/lib3/src/lib/lib3.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-lib3',
+  template: `
+    <p>
+      lib3 works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class Lib3Component {
+
+}
